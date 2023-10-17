2c47dc1905fbe11a6d35b0ee28760643d58cfe3f
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-17 22:41:57 +0530

    Setup redux toolkit store and dispatch actions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
 
 
 function App() {
+  const dispatch = useDispatch();
   return (
     <div className="dark">
+      <button
+      onClick={()=> {
+        dispatch(logout());
+      }}
+      >logout</button>
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>} />
