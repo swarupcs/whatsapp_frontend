aed6a86ffe7d6513de19d68d61a362ec13fb8450
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-10-23 19:43:31 +0530

    Setup the register and login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import LoginForm from '../components/auth/LoginForm'
 
 function login() {
   return (
-    <div>
-      login
+    <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      {/* Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        {/* Login Form  */}
+        <LoginForm/>
+      </div>
+     
     </div>
   )
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm'
 
 function register() {
   return (
-    <div>
-      register
+    <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      {/* Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+      {/* Register Form  */}
+      <RegisterForm/>   
+      </div>
+  
     </div>
   )
 }
